403cfad95ec758b0a86dfbb93c35f27193fd2d36
catatjinky <126514932+catatjinky@users.noreply.github.com>
2023-04-03 21:06:10 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+console.log("connected");
+
+let a = 5;
+let b = 10;
+
+console.log(a < b); // output: true
+
+a += 3;
+console.log(a < b); // output: false
+
+b *= 2;
+console.log(a <= b); // output: true
+console.log(a !== b); // output: true
+console.log(a >= b); // output: false
